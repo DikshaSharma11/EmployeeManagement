d31bc7ceedb68e87da7c93ec9d53bebdc3a4ed4a
Diksha Sharma <diksha@softobiz.com>
2024-07-13 19:22:15 +0530

    create models for department, user and employee

---
+++ Backend/models/employee.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const EmployeeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  departmentId: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+module.exports = mongoose.model("Employee", EmployeeSchema);
